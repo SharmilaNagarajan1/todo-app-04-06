daeef974be9e67020ab0faf0ed37d741a7c58101
Sharmila Nagarajan <sharmeeh@outlook.com.au>
2025-06-05 11:01:54 +1000

    Update variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "resource_group_name" {
+  default = " "
+}
+
+variable "location" {
+  default = " "
+}
+
+variable "app_service_plan_name" {
+  default = " "
+}
+
+variable "frontend_app_name" {
+  default = "frontend-app"
+}
+
+variable "backend_app_name" {
+  default = "backend-app"
+}
+
+variable "cosmosdb_account_name" {
+  default = "todocosmosaccount"
+}
+
+variable "cosmosdb_db_name" {
+  default = "tododb"
+}
