f6687c2be9346e467fc0ed9277ed07faa18d05ce
Sharmila Nagarajan <sharmeeh@outlook.com.au>
2025-06-05 11:04:52 +1000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,31 @@ data "azurerm_resource_group" "rg" {
   name = "existing"
 }
 
+
+resource "azurerm_service_plan" "service-plan" {
+  name                = "demo-az-sp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "webapp1" {
+  name                = "login-app-service"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.service-plan.id
+
+  site_config {}
+}
+
+resource "azurerm_linux_web_app" "webapp2" {
+  name                = "checkout-app-service"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.service-plan.id
+
+  site_config {}
+}
+
+
